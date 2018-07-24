714e793649821fc658dc74001e8c7e13c652d7f2
ng6fd11 <tom.langfeldt.external@airbus.com>
2018-07-24 16:36:30 +0200

    Adding GeoPoint class in order to handle geo coordinates and convertion from geographic coordinates to cartesian coordinates

---
+++ app/src/main/java/fr/cnam/nsy209/artour2/location/GeoPoint.java
@@ -0,0 +1,57 @@
+package fr.cnam.nsy209.artour2.location;
+
+
+import com.google.ar.core.Pose;
+
+import java.util.Locale;
+
+public class GeoPoint {
+
+    public static double LATITUDE_LENGTH    = 110570;
+    public static double LONGITUDE_LENGTH   = 111320;
+
+    private double m_Latitude;
+    private double m_Longitude;
+
+    private static String EXCEPTION_TAG = "GeoPoint";
+
+    public double getLatitude   () { return this.m_Latitude; }
+    public double getLongitude  () { return this.m_Longitude;}
+
+    public void setLatitude(double p_Latitude) throws Exception {
+        if (p_Latitude > 90F || p_Latitude < -90F)
+            throw new Exception(String.format(Locale.getDefault(),
+                    "%s - Bad latitude passed as argument to constructor -> latitude: %d",
+                    EXCEPTION_TAG, p_Latitude));
+    }
+
+    public void setLongitude(double p_Longitude) throws Exception {
+        if (p_Longitude > 90F || p_Longitude < -90F)
+            throw new Exception(String.format(Locale.getDefault(),
+                    "%s - Bad longitude passed as argument to constructor -> latitude: %d",
+                    EXCEPTION_TAG, p_Longitude));
+    }
+
+    public GeoPoint(double p_Latitude, double p_Longitude) throws Exception {
+        this.setLatitude(p_Latitude);
+        this.setLongitude(p_Longitude);
+    }
+
+    public GeoPoint subtract(GeoPoint p_Operand) throws Exception {
+        return new GeoPoint(p_Operand.getLatitude() - this.getLatitude(), p_Operand.getLongitude() - this.getLongitude());
+    }
+
+    public GeoPoint add(GeoPoint p_Operand) throws Exception {
+        return new GeoPoint(p_Operand.getLatitude() + this.getLatitude(), p_Operand.getLongitude() + this.getLongitude());
+    }
+
+    public Pose getRelativePose(GeoPoint p_Operand) throws Exception {
+        GeoPoint l_Difference = this.subtract(p_Operand);
+
+        float l_Latitude = (float)(LATITUDE_LENGTH * l_Difference.getLatitude());
+        float l_Longitude = (float)(LONGITUDE_LENGTH * l_Difference.getLongitude() * Math.cos(l_Latitude));
+
+        return new Pose(new float[]{l_Latitude, l_Longitude, 0f}, new float[]{0f, 0f, 0f});
+    }
+
+}
